f58b6eca8dd9091e05f747944be638d10d5f09fa
AbdulAlt <lancerdeltarune8@gmail.com>
2025-02-23 20:06:08 +0000

    Add files via upload

---
+++ hmwk2.py
@@ -0,0 +1,23 @@
+class Car:
+    def __init__(self, brand, color, speed):
+        self.brand = brand
+        self.color = color
+        self.speed = speed
+
+    def show_info(self):
+        print("Brand: {}, Color: {}, Speed: {} mph".format(self.brand, self.color, self.speed))
+
+    def drive(self):
+        print("The car is moving!")
+
+# Creating two car objects
+car1 = Car("Toyota", "Red", 100)
+car2 = Car("Ford", "Blue", 120)
+
+# Calling show_info for both cars
+car1.show_info()
+car2.show_info()
+
+# Calling drive method
+car1.drive()
+car2.drive()
