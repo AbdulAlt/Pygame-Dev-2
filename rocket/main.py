a53493cf483852eda1f08cb04be7369b9485534d
AbdulAlt <lancerdeltarune8@gmail.com>
2025-04-13 19:19:14 +0100

    Update main.py

--- rocket/main.py
+++ rocket/main.py
@@ -13,16 +13,58 @@ center = (WIDTH / 2, HEIGHT / 2)
 player_x = 200
 player_y = 200
 keys = [False, False, False, False]
-player = pygame.image.load("C:\Users\44794\Music\Game Dev 2\images\rocket")
-bg = pygame.image.load("bg.png")
+player = pygame.image.load("images/rocket.png")
+bg = pygame.image.load("images/bg.png")
 
 while player_y < 600:
     # blit the images
     screen.blit(bg, (0, 0))
     screen.blit(player, (player_x, player_y))
     pygame.display.update()
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            
-    pygame.display.update()
+            exit(0)
+
+        # detect player input
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                keys[0] = True
+            elif event.key == pygame.K_LEFT:
+                keys[1] = True
+            elif event.key == pygame.K_DOWN:
+                keys[2] = True
+            elif event.key == pygame.K_RIGHT:
+                keys[3] = True
+        
+        # if key released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                keys[0] = False
+            elif event.key == pygame.K_LEFT:
+                keys[1] = False
+            elif event.key == pygame.K_DOWN:
+                keys[2] = False
+            elif event.key == pygame.K_RIGHT:
+                keys[3] = False
+
+    # movement
+    if keys[0]:
+        if player_y > 0:
+            player_y -= 7
+    elif keys[2]:
+        if player_y < 536:
+            player_y += 7
+    elif keys[1]:
+        if player_x > 0:
+            player_x -= 2
+    elif keys[3]:
+        if player_x < 536:
+            player_x += 2
+
+
+    player_y += 5
+    sleep(0.05)
+
+print("Game Over!")
