3df2196aca19501d1fb87ca00ce6944d862ed2b7
AbdulAlt <lancerdeltarune8@gmail.com>
2025-05-25 18:53:52 +0100

    Update main.py

--- sprite_rockets/main.py
+++ sprite_rockets/main.py
@@ -14,10 +14,9 @@ class Player(pygame.sprite.Sprite):
         self.rocket = pygame.transform.scale(self.rocket, (70, 100))
         self.rect = self.rocket.get_rect()
         self.image = self.rocket
+        self.rect.center = (WIDTH // 2, HEIGHT // 2)
 
-    # update function
     def update(self, pressed_key):
-        # input
         if pressed_key[pygame.K_UP]:
             self.rect.move_ip(0, -5)
         if pressed_key[pygame.K_DOWN]:
@@ -26,8 +25,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.move_ip(-5, 0)
         if pressed_key[pygame.K_RIGHT]:
             self.rect.move_ip(5, 0)
-        
-        # limits
+
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.right > WIDTH:
@@ -37,28 +35,85 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom >= HEIGHT:
             self.rect.bottom = HEIGHT
 
+# Apple class
+class Apple(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.apple = pygame.image.load("images/apple.png").convert_alpha()
+        self.apple = pygame.transform.scale(self.apple, (50, 50))
+        self.rect = self.apple.get_rect()
+        self.image = self.apple
+        self.rect.topleft = (100, 100)
 
-sprites = pygame.sprite.Group()
+# Banana class
+class Banana(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.banana = pygame.image.load("images/banana.png").convert_alpha()
+        self.banana = pygame.transform.scale(self.banana, (50, 50))
+        self.rect = self.banana.get_rect()
+        self.image = self.banana
+        self.rect.topleft = (200, 150)
+
+# Orange class
+class Orange(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.orange = pygame.image.load("images/orange.png").convert_alpha()
+        self.orange = pygame.transform.scale(self.orange, (50, 50))
+        self.rect = self.orange.get_rect()
+        self.image = self.orange
+        self.rect.topleft = (300, 200)
+
+# Strawberry class
+class Strawberry(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.strawberry = pygame.image.load("images/strawberry.png").convert_alpha()
+        self.strawberry = pygame.transform.scale(self.strawberry, (50, 50))
+        self.rect = self.strawberry.get_rect()
+        self.image = self.strawberry
+        self.rect.topleft = (400, 250)
+
+# Watermelon class
+class Watermelon(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.watermelon = pygame.image.load("images/watermelon.png").convert_alpha()
+        self.watermelon = pygame.transform.scale(self.watermelon, (50, 50))
+        self.rect = self.watermelon.get_rect()
+        self.image = self.watermelon
+        self.rect.topleft = (500, 300)
 
 # start game function
 def start_game():
-    # add object to game loop
     player = Player()
-    sprites.add(player)
 
-    # while loop
+    apple = Apple()
+    banana = Banana()
+    orange = Orange()
+    strawberry = Strawberry()
+    watermelon = Watermelon()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                return
 
         pressed_key = pygame.key.get_pressed()
         player.update(pressed_key)
-        
+
         screen.blit(pygame.image.load("images/bg.png"), (0, 0))
-        sprites.draw(screen)
-        
-                
+
+        # draw fruits
+        screen.blit(player.image, player.rect)
+        screen.blit(apple.image, apple.rect)
+        screen.blit(banana.image, banana.rect)
+        screen.blit(orange.image, orange.rect)
+        screen.blit(strawberry.image, strawberry.rect)
+        screen.blit(watermelon.image, watermelon.rect)
+
         pygame.display.update()
 
 start_game()
